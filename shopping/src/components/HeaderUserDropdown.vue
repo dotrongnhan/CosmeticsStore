c58a46bddeea2adb0435b6e445fedecc48585e70
doanlan96 <77398030+doanlan96@users.noreply.github.com>
2021-07-14 16:44:25 +0700

    user drop

---
+++ shopping/src/components/HeaderUserDropdown.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="header-wrapicon2" @mouseover="toggleUserDropdown" @mouseleave="toggleUserUp">
+      <router-link :to="isLoginSuccess ? '/user' : '/login'">
+            <img
+              :src="user?.avatar ? user.avatar : defaultAvatar"
+              class="header-icon1"
+              style="width: 27px; border-radius: 50%"
+              alt="Avatar"
+            />
+      </router-link>     
+    <div
+      class="header-cart header-dropdown"
+      :class="{ 'show-header-dropdown': isShowUserDropdown && isLoginSuccess }"
+    >
+    <div class="dropdown-content">
+    <a href="/user/profile">Profile</a>
+    <a href="/user/settings">Settings</a>
+    <a href="#" @click="logout">Sign out</a>
+  </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import defaultAvatar from "@/assets/images/icons/icon-header-01.png";
+import { mapState, mapMutations, mapActions } from "vuex";
+import { currency } from "@/utils/currency";
+
+export default {
+  name: "HeaderUserDropdown",
+  data() {
+      return { 
+          defaultAvatar
+      }
+  },
+  computed: {
+    // ...mapState("cart", ["products", "isShowCartDropdown", "addToCartResult"]),
+    ...mapState("users", ["isShowUserDropdown", "isLoginSuccess", "user"]),
+    // ...mapGetters("cart", ["totalItems", "subTotal"]),
+  },
+
+  methods: {
+    // getCookie,
+    ...mapActions("users",["getUserExits"]),
+    ...mapMutations("users", ["updateUser"]),
+    toggleUserDropdown() {
+      this.$store.commit("users/setShowUserDropdown", true);
+    },
+    toggleUserUp() {
+      this.$store.commit("users/setShowUserDropdown", false);
+    },
+    logout(event) {
+      event.preventDefault();
+      this.$store.dispatch("users/logout");
+      this.$router.replace("/");
+    },
+    currency,
+  },
+};
+</script>
+
+<style scoped>
+.header-cart {
+    margin-right: -30px;
+    width: 140px;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+        background-color: lightgrey;
+}
+</style>
