4e837b7c9b539000b08a2db3faa03f664c88a007
doanlan96 <77398030+doanlan96@users.noreply.github.com>
2021-07-17 16:57:51 +0700

    Update user.go

--- Testgorrilamux/models/user.go
+++ Testgorrilamux/models/user.go
@@ -12,3 +12,15 @@ type User struct {
 	Avatar      string `json:"avatar"`
 	RoleId      uint   `json:"role_id"`
 }
+
+type Customer struct {
+	Id          uint   `json:"id"`
+	FullName    string `json:"full_name"`
+	Email       string `json:"email"`
+	Phone       string `json:"phone"`
+	Address     string `json:"address"`
+	DateOfBirth string `json:"date_of_birth"`
+	Gender      string `json:"gender"`
+	Avatar      string `json:"avatar"`
+	RoleId      uint   `json:"role_id"`
+}
